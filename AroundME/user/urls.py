36973281500626485a9a32ed552f2d48d7e63360
Sree Ram M S <93707684+Sree-Ram-MS@users.noreply.github.com>
2023-02-24 10:38:42 +0000

    24/02/23

--- AroundME/user/urls.py
+++ AroundME/user/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import UserHome
+from .views import UserHome,Profile
 
 urlpatterns = [
-    path("userpage/",UserHome.as_view(),name="userpage")
+    path("userpage/",UserHome.as_view(),name="userpage"),
+    path("profie/",Profile.as_view(),name="Profile")
 ]
--- AroundME/user/views.py
+++ AroundME/user/views.py
@@ -4,3 +4,6 @@ from django.views.generic import View,TemplateView,CreateView
 # Create your views here.
 class UserHome(TemplateView):
     template_name="Userpage.html"
+
+class Profile(TemplateView):
+    template_name="UserProfile.html"
