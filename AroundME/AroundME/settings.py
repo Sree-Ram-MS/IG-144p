0574c367dc667b8761ef415a025696885c9ceee1
Sree Ram M S <93707684+Sree-Ram-MS@users.noreply.github.com>
2023-02-25 19:29:17 +0000

    .

--- AroundME/AroundME/settings.py
+++ AroundME/AroundME/settings.py
@@ -129,3 +129,6 @@ CSRF_TRUSTED_ORIGINS = [
     'https://sree-ram-ms-upgraded-spoon-xq76pwwx7xwf99v-8000.preview.app.github.dev',
     'https://sree-ram-ms-upgraded-spoon-xq76pwwx7xwf99v.github.dev'
 ]
+
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# MEDIA_URL = '/media/'
--- AroundME/user/urls.py
+++ AroundME/user/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from .views import UserHome,Profile,LogOut
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("userpage/",UserHome.as_view(),name="userpage"),
     path("profie/",Profile.as_view(),name="Profile"),
     path("logout/",LogOut.as_view(),name="Logout")
-]
-]
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
