ec7793a20f7c6b39915bfb853fcb20ea9809d0c0
Sree Ram M S <93707684+Sree-Ram-MS@users.noreply.github.com>
2023-02-25 18:30:10 +0000

    .

--- AroundME/user/urls.py
+++ AroundME/user/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import UserHome,Profile
+from .views import UserHome,Profile,LogOut
 
 urlpatterns = [
     path("userpage/",UserHome.as_view(),name="userpage"),
-    path("profie/",Profile.as_view(),name="Profile")
+    path("profie/",Profile.as_view(),name="Profile"),
+    path("logout/",LogOut.as_view(),name="Logout")
+]
 ]
--- AroundME/user/views.py
+++ AroundME/user/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.views.generic import View,TemplateView,CreateView
+from django.contrib.auth import logout
 
 # Create your views here.
 class UserHome(TemplateView):
@@ -7,3 +8,8 @@ class UserHome(TemplateView):
 
 class Profile(TemplateView):
     template_name="UserProfile.html"
+
+class LogOut(View):
+    def get(self,req):
+        logout(req)
+        return redirect("Homepage")
